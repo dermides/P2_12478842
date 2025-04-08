f0162eed45a9b13c8b3812031be73466c3e7fc4d
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-04-08 16:40:23 -0400

    Configuracion render

--- app.ts
+++ app.ts
@@ -8,9 +8,9 @@ const dotenv = require("dotenv")
 dotenv.config();
 const app = express();
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname,"views"));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use('/', routerMain);
 
