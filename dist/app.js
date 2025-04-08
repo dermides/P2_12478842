abf67daf871d5a0fb5f3bcfa6ed85c12c057379d
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-04-08 15:44:40 -0400

    Configurando render

---
+++ dist/app.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const home_1 = require("./routes/home");
+const ejs = require('ejs');
+const path = require('path');
+const dotenv = require("dotenv");
+dotenv.config();
+const app = (0, express_1.default)();
+app.set('views', path.join(__dirname, 'src/views'));
+app.set("view engine", "ejs");
+app.use(express_1.default.static(path.join(__dirname, "public")));
+app.use('/', home_1.routerMain);
+app.use((req, res, next) => {
+    res.status(404).render("404", { titulo: "Página 404" });
+});
+app.use(express_1.default.json());
+const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
+app.listen(PORT, () => {
+    return console.log(`http://localhost:${PORT}`);
+});
+//# sourceMappingURL=app.js.map
---
+++ dist/routes/home.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.routerMain = void 0;
+const express_1 = require("express");
+exports.routerMain = (0, express_1.Router)();
+exports.routerMain.get('/', (req, res) => {
+    res.render('index', { titulo: "Sistema de alquiler" });
+});
+//# sourceMappingURL=home.js.map
