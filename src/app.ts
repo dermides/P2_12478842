f14d9047d2e30cec213cdbd8f3a3b8a6d8af8342
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-04-08 17:05:35 -0400

    Views

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const dotenv = require("dotenv")
 dotenv.config();
 const app = express();
 
-app.set('views', path.join(__dirname,"views"));
+app.set('views', path.join(__dirname,"src/views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
 
