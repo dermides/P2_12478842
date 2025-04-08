8e9623f5ee384a90cd527b7e27ecfe9adb16ee4d
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-04-08 17:05:45 -0400

    Build

--- dist/src/app.js
+++ dist/src/app.js
@@ -11,7 +11,7 @@ const path = require('path');
 const dotenv = require("dotenv");
 dotenv.config();
 const app = (0, express_1.default)();
-app.set('views', path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 app.use(express_1.default.static(path.join(__dirname, "public")));
 app.use('/', home_1.routerMain);
