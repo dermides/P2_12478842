60ac0e4493c5bbd78181b4af8786669d9067feb7
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-04-08 17:02:22 -0400

    Aplicando render

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { routerMain } from './routes/home';
+import { routerMain } from '../routes/home';
 
 const ejs = require('ejs');
 const path = require('path');
