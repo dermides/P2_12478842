5b219a7ad9be17875bdf2ca8e7331b7ff4924383
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-04-08 15:55:31 -0400

    Actualizando render

--- dist/app.js
+++ dist/app.js
@@ -11,9 +11,9 @@ const path = require('path');
 const dotenv = require("dotenv");
 dotenv.config();
 const app = (0, express_1.default)();
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', __dirname + '/src/views');
 app.set("view engine", "ejs");
-app.use(express_1.default.static(path.join(__dirname, "public")));
+app.use(express_1.default.static(__dirname + "/public"));
 app.use('/', home_1.routerMain);
 app.use((req, res, next) => {
     res.status(404).render("404", { titulo: "Página 404" });
