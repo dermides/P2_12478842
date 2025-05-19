82ad4854da08a606222adf2252bcaf8f139e4b40
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-05-19 18:37:37 -0400

    Actualizando

--- src/public/assets/js/analytics.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8N492X39JK"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-8N492X39JK');
-</script>
