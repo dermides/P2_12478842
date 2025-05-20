f73215aeb516f82d204399a67281cc2002c4fe54
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-05-20 14:35:36 -0400

    Configurar server email

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: parseInt(process.env.MAIL_PORT || "2525"),
+  port: parseInt(process.env.MAIL_PORT || "587"),
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
