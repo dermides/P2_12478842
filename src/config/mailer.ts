ce1234f658fa4a404c6914ea3ee490741704805a
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-05-20 15:03:22 -0400

    Configurar Email con gmail

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -5,7 +5,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: parseInt(process.env.MAIL_PORT || "587"),
+  port: parseInt(process.env.MAIL_PORT || "465"),
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
