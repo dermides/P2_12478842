fe7ed070e8a2c7413d0ae8d7f25cac6b7e625502
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-05-20 16:30:09 -0400

    Configurando el pago

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import paymentModel from '../models/paymentModel';
 import { validationResult } from 'express-validator';
-
+import axios from 'axios';
 
 class paymentController {
 
@@ -9,19 +9,41 @@ class paymentController {
     const pagos = await paymentModel.findAll();
     res.render('admin/listpagos', { pagos });
   }
-  
+
+  async procesarPago(monto: number, tarjeta: string, cvv: string, mes: string, ano:string) {
+    try {
+      const response = await axios.post(`${process.env.PAYMENT_API_URL}/pay`, {
+        amount: monto,
+        "card-number": tarjeta,
+        cvv: cvv,
+        "expiration-month": mes,
+        "expiration-year": ano
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error al procesar el pago:", error);
+      return null;
+    }
+  }
+
   async addPayment(req: Request, res: Response) {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        const valores = req.body
-        const validaciones = errors.array()
-        res.render('home/pagos', {message: req.flash(), validaciones:validaciones, valores: valores})
+      const valores = req.body
+      const validaciones = errors.array()
+      res.render('home/pagos', { message: req.flash(), validaciones: validaciones, valores: valores })
     } else {
       const { nombre_titular, email, numero_tarjeta, month, year, codigo_seguridad, monto, moneda } = req.body;
-      await paymentModel.create(nombre_titular, email, numero_tarjeta, month, year, codigo_seguridad, monto, moneda);
-      
-      req.flash('success', '¡Pago realizado exitosamente!');
-      res.redirect('/pagos'); // Redirigir a la ruta /contact/add después de agregar el contacto
+      const resultado = await this.procesarPago(monto, numero_tarjeta, codigo_seguridad, month, year);
+
+      if (resultado) {
+        await paymentModel.create(nombre_titular, email, numero_tarjeta, month, year, codigo_seguridad, monto, moneda);
+        req.flash('success', '¡Pago realizado exitosamente!');
+        res.redirect('/pagos');
+      } else {
+        req.flash('success', '¡Pago no realizado!');
+        res.redirect('/pagos');
+      }
 
     }
 
@@ -29,3 +51,4 @@ class paymentController {
 }
 
 export default new paymentController();
+
