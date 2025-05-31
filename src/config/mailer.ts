0afaefb5c28956506a5f38497c5711eda6c09943
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-05-31 09:00:08 -0400

    Puerto ssl en correo

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: parseInt(process.env.MAIL_PORT || "2525"),
-  secure: process.env.MAIL_SECU,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
