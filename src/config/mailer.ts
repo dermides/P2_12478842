ff905ada154a6e0023308fda648e0fef765168f4
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-05-31 08:58:34 -0400

    Probando puerto de correo

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: parseInt(process.env.MAIL_PORT || "2525"),
-  secure: false,
+  secure: process.env.MAIL_SECU,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
