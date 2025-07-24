0ce087119d0e429187c09db847ef8f7b7c7f798a
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-07-24 15:57:05 -0400

    Update contactsRoute.ts

--- src/routes/contactsRoute.ts
+++ src/routes/contactsRoute.ts
@@ -3,7 +3,7 @@ import ContactsController from '../controllers/ContactsController';
 
 const router = express.Router();
 
-//router.get('/contactos', ContactsController.getContacts);
+router.get('/contactos', ContactsController.getContacts);
 router.post('/contact/add', ContactsController.addContact);
 
 export default router;
