92b156bf103703cf9e0a767495fb1f8f56355c14
Dermides Blanco <39437160+dermides@users.noreply.github.com>
2025-07-24 15:58:27 -0400

    Update admin.ts

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -17,9 +17,9 @@ router.post('/admin/login', authController.userPass);
 
 //router.get('/admin',adminController.panelAdmin);
 
-router.get('/admin/contactos', ContactsController.indexContactos);
+router.get('/admin/contactos', isAuthenticated, ContactsController.indexContactos);
 router.get('/admin/pagos', isAuthenticated, paymentController.indexPagos);
-router.get('/admin/registro', authController.userRegistro);
+router.get('/admin/registro', isAuthenticated, authController.userRegistro);
 router.post('/admin/registro/add', authController.userAdd);
 
 router.post('/admin/logout', (req, res) => {
